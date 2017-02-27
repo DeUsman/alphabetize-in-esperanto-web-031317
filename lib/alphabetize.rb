2951c3abecf25ec8bab9c9115baa552676fea781
Usman Pervaiz <de.usman@yahoo.com>
2017-02-27 12:13:38 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+    arr.sort_by do |expressions|
+      expressions.chars.map do |character|
+        alpha.index(character)
+      end
+    end
 end
